366f05b7f4d6cc0906b4d17fe5908e9f1f75b187
devjunayed <devjunayed@gmail.com>
2024-10-22 14:55:32 +0600

    unhandled and uncaught excpetion handling done

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,34 @@
-import app from './app'
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+import { Server } from "http";
 
-app.listen(5000, ()=> {
-    console.log('server is running')
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    server = app.listen(config.port, () => {
+      console.log("server is running");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+process.on('unhandledRejection', () => {
+    console.log('unhandledRejection is detected, shutting down')
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
 })
+
+process.on('uncaughtException', () => {
+    console.log('uncaughtException is detected, shutting down');
+    process.exit(1)
+})
+
+main();
