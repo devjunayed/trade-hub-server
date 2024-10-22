6d0ecff9dbfc6ddd2f562299fa9cfa98f22300f2
devjunayed <devjunayed@gmail.com>
2024-10-22 22:05:53 +0600

    validate request middleware is creaetd

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
-import {Router} from 'express';
-import { UserController } from './user.controller';
+import { Router } from "express";
+import { UserController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import userValidationSchema from "./user.validation";
 
 const router = Router();
 
-router.post('/create-user', UserController.createUser);
+router.post(
+  "/create-user",
+  validateRequest(userValidationSchema),
+  UserController.createUser
+);
 
-export const UserRoutes =  router;
+export const UserRoutes = router;
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+const userValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    }),
+    email: z
+      .string({
+        required_error: "Email is required",
+        invalid_type_error: "Email must be a string",
+      })
+      .email("Invalid email address format"),
+    address: z.string({
+      required_error: "Address is required",
+      invalid_type_error: "Address must be a string",
+    }),
+    password: z.string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    }),
+    phone: z
+      .string({
+        required_error: "Phone is required",
+        invalid_type_error: "Phone must be a string",
+      })
+      .regex(/^\+?[1-9]\d{1,14}$/, "Invalid phone number format"),
+    role: z
+      .enum(["user", "admin"], {
+        required_error: "Role is required",
+        invalid_type_error: 'Role must be either "user" or "admin"',
+      })
+      .default("user"),
+    isDeleted: z.boolean().optional().default(false),
+  }),
+});
+
+export default userValidationSchema;
