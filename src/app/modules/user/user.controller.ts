2f9af4d1d81241f9110d7bb97deb0fb12ad8c1c4
devjunayed <devjunayed@gmail.com>
2024-10-22 20:12:12 +0600

    send Response utils function created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,18 @@
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { UserServices } from "./user.service";
 
 const createUser = catchAsync(async (req, res) => {
-    const result = await UserServices.createUserIntoDB(req.body);
+  const result = await UserServices.createUserIntoDB(req.body);
 
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User created successfully!",
+    data: result,
+  });
 });
 
 export const UserController = {
-    createUser
-}
+  createUser,
+};
---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,34 @@
+import { Response } from "express";
+import httpStatus from "http-status";
+
+type TResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  token?: string;
+  message: string;
+  data: T;
+};
+export function sendResponse<T>(
+  res: Response,
+  { statusCode, success, token, message, data }: TResponse<T>
+) {
+  const responseData: TResponse<T> = {
+    success,
+    statusCode,
+    message,
+    data,
+  };
+
+  if (token) {
+    responseData.token = token;
+  }
+
+  if ((Array.isArray(data) && data?.length === 0) || !data) {
+    responseData.data = (Array.isArray(data) ? [] : null) as T;
+    responseData.message = "No Data Found";
+    responseData.statusCode = httpStatus.NOT_FOUND;
+    responseData.success = false;
+  }
+
+  res.status(responseData.statusCode).json(responseData);
+}
