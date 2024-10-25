161e3e4443ebf5af765c4b1f1246620476b0f2fc
devjunayed <devjunayed@gmail.com>
2024-10-25 22:02:30 +0600

    product zod validation schema is created

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -1,74 +1,77 @@
 import { z } from "zod";
 
-const createUserValidationSchema = z.object({
+const createProductValidationSchema = z.object({
   body: z.object({
     name: z.string({
       required_error: "Name is required",
       invalid_type_error: "Name must be a string",
     }),
-    email: z
-      .string({
-        required_error: "Email is required",
-        invalid_type_error: "Email must be a string",
-      })
-      .email("Invalid email address format"),
-    address: z.string({
-      required_error: "Address is required",
-      invalid_type_error: "Address must be a string",
+    description: z.string({
+      required_error: "Description is required",
+      invalid_type_error: "Description must be a string",
     }),
-    password: z.string({
-      required_error: "Password is required",
-      invalid_type_error: "Password must be a string",
+    price: z.number({
+      required_error: "Price is required",
+      invalid_type_error: "Price must be a number",
     }),
-    phone: z
+    stockQuantity: z.number({
+      required_error: "Stock quantity is required",
+      invalid_type_error: "Stock quantity must be a number",
+    }),
+    category: z.string({
+      required_error: "Category is required",
+      invalid_type_error: "Category must be a string",
+    }),
+    productImage: z.string({
+      required_error: "Product Image is required",
+      invalid_type_error: "Product Image must be a string url",
+    }),
+    isDeleted: z.boolean().optional().default(false),
+  }),
+});
+const updateProductValidationSchema = z.object({
+  body: z.object({
+    name: z
       .string({
-        required_error: "Phone is required",
-        invalid_type_error: "Phone must be a string",
+        required_error: "Name is required",
+        invalid_type_error: "Name must be a string",
       })
-      .regex(/^\+?[1-9]\d{1,14}$/, "Invalid phone number format"),
-    role: z
-      .enum(["user", "admin"], {
-        required_error: "Role is required",
-        invalid_type_error: 'Role must be either "user" or "admin"',
+      .optional(),
+    description: z
+      .string({
+        required_error: "Description is required",
+        invalid_type_error: "Description must be a string",
       })
-      .default("user"),
+      .optional(),
+    price: z
+      .number({
+        required_error: "Price is required",
+        invalid_type_error: "Price must be a number",
+      })
+      .optional(),
+    stockQuantity: z
+      .number({
+        required_error: "Stock quantity is required",
+        invalid_type_error: "Stock quantity must be a number",
+      })
+      .optional(),
+    category: z
+      .string({
+        required_error: "Category is required",
+        invalid_type_error: "Category must be a string",
+      })
+      .optional(),
+    productImage: z
+      .string({
+        required_error: "Product Image is required",
+        invalid_type_error: "Product Image must be a string url",
+      })
+      .optional(),
     isDeleted: z.boolean().optional().default(false),
   }),
 });
 
-const updateUserValidationSchema = z.object({
-  body: z.object({
-    name: z
-      .string({
-        invalid_type_error: "Name must be a string",
-      })
-      .optional(),
-    email: z
-      .string({
-        invalid_type_error: "Email must be a string",
-      })
-      .email("Invalid email address format")
-      .optional(),
-    address: z
-      .string({
-        invalid_type_error: "Address must be a string",
-      })
-      .optional(),
-    password: z
-      .string({
-        invalid_type_error: "Password must be a string",
-      })
-      .optional(),
-    phone: z
-      .string({
-        invalid_type_error: "Phone must be a string",
-      })
-      .regex(/^\+?[1-9]\d{1,14}$/, "Invalid phone number format")
-      .optional(),
-  }),
-});
-
-export const userValidation = {
-  createUserValidationSchema,
-  updateUserValidationSchema,
+export const productValidation = {
+  createProductValidationSchema,
+  updateProductValidationSchema,
 };
